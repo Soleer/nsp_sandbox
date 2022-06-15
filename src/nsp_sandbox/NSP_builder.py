424191664ff7c485628c83cad1e5ae24cc755913
Niklas Wünstel <wuenstel@stud.uni-heidelberg.de>
2022-06-15 23:37:35 +0200

    basic variables

--- src/nsp_sandbox/NSP_builder.py
+++ src/nsp_sandbox/NSP_builder.py
@@ -1,7 +1,22 @@
+from tomlkit import integer, string
+
+
 class NSPbuilder:
     print_info: bool
-
+    version: str
+    n_nurses: int
+    n_jobs: int
+    n_days: int
+    
     def __init__(self, print_info=True) -> None:
         self.print_info = print_info
         self.version = "0.0.1"
     
+    def set_n_nurses(self, n:int) -> None:
+        self.n_nurses = n
+    
+    def set_n_jobs(self, n:int) -> None:
+        self.n_jobs = n
+    
+    def set_n_days(self, n:int) -> None:
+        self.n_days = n
--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,6 +1,8 @@
 import pytest
 from nsp_sandbox.NSP_builder import NSPbuilder
 
+version = "0.0.1"
 def test_init():
     builder = NSPbuilder()
     assert builder.print_info, "Printing should be done by default."
+    assert builder.version == version, "Version sould be " + version
